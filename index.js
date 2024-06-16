999d7f5f54af5626e777644da2196122d58fc3aa
justinwang <justinwang407@gmail.com>
2024-06-15 23:48:59 -0400

    Added enter key functionality

--- index.js
+++ index.js
@@ -17,18 +17,22 @@ function updateBoardState() { // Function displays changes to the board
 
 document.addEventListener('keydown', function(event) { // Event handler for keyboard
     let input = event.key;
-    if(input.length === 1 && input.match(/[a-z]/i)) { // If the keyboard input is a letter, display it
+    if (input.length === 1 && input.match(/[a-z]/i)) { // If the keyboard input is a letter, display it
         if (wordleState.currentCol === 5) return;
         wordleState.grid[wordleState.currentRow][wordleState.currentCol] = input;
         wordleState.currentCol++;
 
         updateBoardState();
 
-    } else if(input == 'Backspace') { // If the keyboard input is a backspace, remove letter
+    } else if (input == 'Backspace') { // If the keyboard input is a backspace, remove letter
         if (wordleState.currentCol === 0) return;
         wordleState.grid[wordleState.currentRow][wordleState.currentCol - 1] = '';
         wordleState.currentCol--;
 
         updateBoardState();
+
+    } else if (input == 'Enter') {
+        wordleState.currentRow++;
+        wordleState.currentCol = 0;
     }
 });
