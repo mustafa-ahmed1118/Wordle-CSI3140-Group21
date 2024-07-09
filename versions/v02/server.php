ddbb46aef5c9ea722ed2e552f4153c4b48331346
justinwang <justinwang407@gmail.com>
2024-07-09 16:23:32 -0400

    Added random word in php

--- versions/v02/server.php
+++ versions/v02/server.php
@@ -1,3 +1,16 @@
 <?php
-    session_start();
+session_start();
+
+header('Content-Type: application/json');
+
+function getRandomWord() {
+    $words = [
+        "house", "apple", "bread", "earth", "beach",
+        "radio", "river", "tiger", "green", "clock",
+        "train", "shirt", "storm", "flame", "plane",
+        "money", "dance", "music", "photo", "phone",
+        "creed", "trial", "drink", "booze", "crime", "legal"
+    ];
+    return $words[array_rand($words)];
+}
 ?>
